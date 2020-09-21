b9e0e75c2a0802e561c77ca00f3599a119b7526f
Gerardo Gonzales <gerargd@outlook.es>
2020-09-21 02:22:00 -0600

    Missing Threads

--- LRU_Algorithm.cpp
+++ LRU_Algorithm.cpp
@@ -2,123 +2,231 @@
 #include <time.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <stack>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+//#include <pthread.h>
 #include <vector>
+//#include <unistd.h>
 #include <iostream>
+#include <queue> 
 using namespace std;
 
+struct LRU {
+
+    int referencia;
+    int cReferenciado;
+
+};
+
 //Variables Globales
-stack <int> pila;
-vector <int> referenciasPasadas;
+vector <LRU> checkR;
+queue <int> cola;
+int fallosG;
+int memoria[5];
 
-int memoria[20];
-
-void* genRandNum(void*){
+void genRandNum(void*) {
 
     srand(time(NULL));
 
     //El ciclo genera n cantidad de referencias
-    for(int i=0; i<5; i++){
+    for (int i = 0; i < 7; i++) {
 
-        int num = rand() % (101 - 1);       //Generar numeros aleatorios entre 0-100
-        cout<<"Numero de referencia de pagina: "<<num<<endl;
-        pila.push(num);
+        int num = rand() % 20 + 1;       //Generar numeros aleatorios entre 1-100
+        cout << "Numero de referencia de pagina: " << num << endl;
+        cola.push(num);
 
     }
-    
 }
 
-bool checkMem(int arreglo[]){
+bool checkMem(int arreglo[]) {
 
-    int length = (sizeof(arreglo)/ sizeof(*arreglo));
-    bool lleno = false;
+    return memoria[4] != NULL ? true : false;
 
-    //Recorrer arreglo y saber si esta lleno
-    for(int i=0;i<length;i++){
-        if(memoria[i] == NULL){
-            lleno = false;
-        }else{
-            lleno = true;
+}
+
+bool checkRef(int ref, int arreglo[]) {
+
+    bool esta = false;
+
+    for (int i = 0; i < 5; i++) {
+
+        if (memoria[i] == ref) {  //Si la referencia ya esta en memoria
+            return true;
         }
 
     }
 
-    return lleno;
+    return esta;
 
 }
 
-void* getReference(void*){
+int nextPos(int arreglo[]) {
 
-    int rPagina = pila.top();     //Agarro el primero que esta en la pila
-    pila.pop();             //y luego lo saco
+    int i;
 
-    int length = (sizeof(memoria)/ sizeof(*memoria));
+    for (i = 0; i < 20; i++) {
+
+        if (arreglo[i] == NULL) {
+            return i;
+        }
+
+    }
+
+    return -1;
+
+}
+
+void updateRef(int ref) {
+
+    int veces = 0;
+
+    for (int i = 0; i < checkR.size(); i++) {
+
+        if (checkR[i].referencia == ref) {
+            veces++;
+
+        }
+
+    }
+
+    for (int i = 0; i < checkR.size(); i++) {
+
+        if (checkR[i].referencia == ref) {
+
+            checkR[i].cReferenciado = veces;
+        }
+    }
+
+
+
+
+}
+
+int getReferenced(int ref) {
+
+
+
+    for (int i = 0; i < checkR.size(); i++) {
+
+        if (checkR[i].referencia == ref) {
+            return checkR[i].cReferenciado;
+        }
+    }
+
+
+
+}
+
+void* getReference(void*) {
+
+    //cout<<"Entramos al hilo 2..."<<endl;
+    //cout<<"Entramos al hilo 2..."<<endl;
     int fallos = 0;
-    int espaciosAtras = 0;
 
-    for(int i=0;i<length; i++){
-        
-        if(rPagina == memoria[i]){
-            //El numero ya esta en el arreglo
-        }else if(checkMem(memoria)){         //Si el arreglo esta lleno
+    int ref = cola.front();
+    struct LRU toRefer;
+    toRefer.referencia = ref;
+    cola.pop();
 
-            //Buscar cual reemplazar
-            //El menos recientemente usado es el que esta en la ultima posicion del vector 
-            for(int j=0;j<20;j++){
+    //if referencia == memoria[i]
+    if (checkRef(ref, memoria))
+    {
 
-                if(memoria[i] == referenciasPasadas.back()){
+        //El numero ya esta en el arreglo
 
-                    memoria[i] = rPagina;
-                    fallos++;
+        cout << "ya esta" << endl;
+    }
 
+    else if (checkMem(memoria)) { //Si el arreglo esta lleno
+
+        cout << "lleno" << endl;
+        // //Buscar cual reemplazar
+        // //El menos recientemente usado es el que esta en la ultima posicion del vector
+        int numero = getReferenced(memoria[0]);
+        int actual = memoria[0];
+        for (int j = 0; j < 5; j++)
+        {
+            bool tochange = false;
+            for (int i = checkR.size() - 1; i > -1; i--)
+            {
+                
+                if (checkRef(checkR[i].referencia, memoria)) {
+                    //cout << checkR[i].referencia <<":";
+                    if (numero>checkR[i].cReferenciado && actual != ref) {
+                        numero = getReferenced(memoria[j]);
+                        actual = memoria[j];
+                        tochange = true;
+                        break;
+                    }
                 }
-
             }
-
-
-        }else{
-            
-            //La referencia no esta en memoria, la agregamos y incrementamos los fallos
-            memoria[i] = rPagina;
-            referenciasPasadas.push_back(rPagina);  //Meto al vector, esto es auxiliar para saber cuales son mis referencias ya ingresadas. 
-            fallos++;
-
         }
+        
+        for (int p = 0; p < 5; p++) {
+            if (memoria[p] == actual) {
+                cout << "se Reemplazo:" << memoria[p] << "por:" << ref << endl;
+            }
+        }
+    }
+    else
+    {
 
+        //La referencia no esta en memoria, la agregamos y incrementamos los fallos
+        int i = nextPos(memoria);
+        memoria[i] = ref;
+        fallosG++;
+        cout << "La referencia es: " << ref << " y esta en la posicion " << i << endl;
     }
 
-
+    //Meto al vector, esto es auxiliar para saber cuales son mis referencias ya ingresadas. 
+    checkR.push_back(toRefer);
+    updateRef(ref);
+    return NULL;
 }
 
-//Mostrar contenido de la pila
-void showStack(stack <int> s){
+void* printFallos(void*) {
 
-    while(!s.empty()){
+    cout << "Fallos: " << fallosG << endl;
+    return NULL;
+}
 
-        cout<<'\t'<<s.top();
-        s.pop();
+// int main(){
 
+//     pthread_t thread1, thread2, thread3;
+//     int iret1, iret2, iret3;
+
+//     //Creamos hilos independientes
+//     iret1 = pthread_create(&thread1, NULL, genRandNum, (void*) NULL);
+//     iret1 = pthread_create(&thread1, NULL, getReference, (void*) NULL);
+
+//     pthread_join(thread1, NULL);
+//     pthread_join(thread1, NULL);
+//     usleep(30000);   
+
+//     iret1 = pthread_create(&thread1, NULL, printFallos, (void*) NULL);
+
+//     pthread_join(thread1, NULL);
+//     usleep(100000);                 //Hilo 3 espera 10 segundos
+
+//     pthread_exit(NULL);    
+//     return 0;
+
+// }
+
+int main() {
+
+    cout << "Generando referencias..." << endl;
+    genRandNum(NULL);
+    cout << "Referencias generadas..." << endl;
+
+
+    while (!cola.empty()) {
+        getReference(NULL);
     }
 
-    cout<<'\n';
+    printFallos(NULL);
 
-}
-
-int main(){
-
-    pthread_t thread1, thread2;
-    int iret1, iret2;
-
-    //Creamos hilos independientes
-    iret1 = pthread_create(&thread1, NULL, genRandNum, NULL);
-    iret2 = pthread_create(&thread2, NULL, getReference, NULL);
-
-    pthread_join(thread1, NULL);
-
-    cout<<"The stack is: ";
-    showStack(pila);
-
-    pthread_exit(NULL);    
     return 0;
 
 }
