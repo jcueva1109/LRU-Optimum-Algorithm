4892b85973d8cd6dbd397ed928a61215a2d78348
johnniemiralda <johnnie07ml@yahoo.com>
2020-09-20 18:48:28 -0600

    mi pipi es glande (accento chino)

--- Optimo_algoritmo.cpp
+++ Optimo_algoritmo.cpp
@@ -1,1 +1,15 @@
 //si podes ver esto es que la tenes chiquita
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stack>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+//Variables Globales
+stack <int> pila;
+vector <int> referenciasPasadas;
+
+int memoria[20];
